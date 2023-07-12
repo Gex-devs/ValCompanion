245c3f0ed7556505a4047b3fe5b10beaf8b5fa80
Gex-devs <dgexion@outlook.com>
2023-07-12 05:12:25 +0300

    Updated the map endpoint to work for both current match and pre match

--- Controllers/get_map.cs
+++ Controllers/get_map.cs
@@ -20,20 +20,50 @@ namespace ValRestServer.Controllers
         [HttpGet]
         public async Task<ActionResult<string>> GetMap()
         {
-            var preMatchId = await RiotClientHelper.Instance.GetPreMatchId();
+            var preMatchId = String.Empty;   
+            try
+            {
+                preMatchId = await RiotClientHelper.Instance.GetPreMatchId();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            
             var url = $"https://glz-eu-1.eu.a.pvp.net/pregame/v1/matches/{preMatchId}";
 
             _httpClient.DefaultRequestHeaders.Clear();
             _httpClient.DefaultRequestHeaders.Add("X-Riot-Entitlements-JWT", RiotClientHelper.Instance.GetEntitlement());
             _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {RiotClientHelper.Instance.GetAuthorization()}");
 
-            var response = await _httpClient.GetAsync(url);
+            HttpResponseMessage response = null;
 
-            if (response.IsSuccessStatusCode)
+            if (preMatchId != String.Empty)
+            {
+                response = await _httpClient.GetAsync(url);
+            }
+            
+
+             if(response != null && response.IsSuccessStatusCode  )
             {
                 var jsonResponse = await response.Content.ReadAsStringAsync();
                 var mapID = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonResponse)["MapID"].ToString();
                 return Ok(mapID);
+
+            }else {
+                var currentMatchId = await RiotClientHelper.Instance.GetCurrentGameId();
+                Console.WriteLine("current Match id " + currentMatchId);
+                url = $"https://glz-eu-1.eu.a.pvp.net/core-game/v1/matches/{currentMatchId}";
+
+                response = await _httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var mapID = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonResponse)["MapID"].ToString();
+                    return Ok(mapID);
+                }
+
             }
 
             return BadRequest("Failed to retrieve map");
